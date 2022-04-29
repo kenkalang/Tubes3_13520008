bd4158402318e4fc84e7c3661ad111ddf15fc740
danielsalim <ZG#@A'5+uRnz9p>
2022-04-29 19:58:02 +0700

    update

--- src/kmp.go
+++ src/kmp.go
@@ -1,48 +1,50 @@
 package handlers
 
-func BorderFunction(pattern string, M int, fail []int) {
+func BorderFunction(pattern string, M int, startIdx []int) {
 	len := 0
 	k := 1
-	fail[0] = 0
+	startIdx[0] = 0
 
 	for k < M {
 		if pattern[k] == pattern[len] {
 			len++
-			fail[k] = len
+			startIdx[k] = len
 			k++
 		} else {
 			if len != 0 {
-				len = fail[len-1]
+				len = startIdx[len-1]
 			} else {
-				fail[k] = 0
+				startIdx[k] = 0
 				k++
 			}
 		}
 	}
 }
 
-func KMP(pattern string, text string) int { //Return -1 if pattern doesn't exists in text else index where pattern start
-	n := len(text)
-	m := len(pattern)
+func KMP(pattern string, text string) int { \
+	//Return -1 if pattern doesn't exists in text 
+	//else index where pattern starts
+	txtLen := len(text)
+	ptrLen := len(pattern)
 
 	//borderFunction
-	var fail [128]int
-	BorderFunction(pattern, m, fail[:])
+	var startIdx [128]int
+	BorderFunction(pattern, m, startIdx[:])
 
-	i := 0
-	j := 0
+	txtIdx := 0
+	ptrIdx := 0
 	//Looping
-	for i < n {
-		if pattern[j] == text[i] {
-			if j == m-1 {
-				return i - m + 1
+	for txtIdx < txtLen {
+		if pattern[j] == text[txtIdx] {
+			if ptrIdx == ptrLen-1 {
+				return txtIdx - ptrLen + 1
 			}
 			i++
 			j++
-		} else if j > 0 {
-			j = fail[j-1]
+		} else if ptrIdx > 0 {
+			ptrIdx = startIdx[j-1]
 		} else {
-			i++
+			txtIdx++
 		}
 	}
 	return -1
