4390d852311957e49a1b23f02e239b41c02ccfc6
danielsalim <ZG#@A'5+uRnz9p>
2022-04-29 22:10:08 +0700

    bonus test

--- src/kmp.go
+++ src/kmp.go
@@ -1,5 +1,7 @@
 package handlers
 
+import "fmt"
+
 func BorderFunction(pattern string, M int, startIdx []int) {
 	len := 0
 	k := 1
@@ -23,7 +25,7 @@ func BorderFunction(pattern string, M int, startIdx []int) {
 
 func KMP(pattern string, text string) int { \
 	//Return -1 if pattern doesn't exists in text 
-	//else index where pattern starts
+	//else return index where found pattern starts
 	txtLen := len(text)
 	ptrLen := len(pattern)
 
@@ -31,13 +33,17 @@ func KMP(pattern string, text string) int { \
 	var startIdx [128]int
 	BorderFunction(pattern, ptrLen, startIdx[:])
 
+	var similarities [128]int
+
 	txtIdx := 0
 	ptrIdx := 0
+	ptrSmlIdx := 0
 	//Looping
 	for txtIdx < txtLen {
+		GetSimilarities(pattern, text, ptrSmlIdx, txtIdx, similarities[:])
 		if pattern[ptrIdx] == text[txtIdx] {
 			if ptrIdx == ptrLen-1 {
-				return txtIdx - ptrLen + 1
+				return findMaxSimilarities(similarities[:], similarities[0])
 			}
 			txtIdx++
 			ptrIdx++
@@ -47,5 +53,31 @@ func KMP(pattern string, text string) int { \
 			txtIdx++
 		}
 	}
-	return -1
+	return findMaxSimilarities(similarities[:], similarities[0])
+}
+
+func GetSimilarities(pattern string, text string, ptrSmlIdx int, txtIdx int, similarities []int) {
+	txtLen := len(text)
+	ptrLen := len(pattern)
+	currSml := 0
+	
+	for txtIdx < txtIdx + ptrLen {
+		if pattern[ptrSmlIdx] == text[txtIdx] {
+			ptrSmlIdx++
+			txtIdx++
+			currSml++
+		} else {
+			ptrSmlIdx++
+			txtIdx++
+		}
+	}
+	similarities[txtIdx] = (currSml/ptrLen)*100
+}
+
+func findMaxSimilarities(similarities []int, max int) {
+	for _, value := range similarities {
+		if value > max {
+			max = value
+		}
+	}
 }
